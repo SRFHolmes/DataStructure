d0537517e8d777363c35e86d6593a16cd4b7534a
zkangHUST <1473345713@qq.com>
2016-08-09 12:51:18 +0800

    queue.h

---
+++ Graph/queue.h
@@ -0,0 +1,15 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+typedef int MyType;
+struct a
+{
+    MyType node[100];
+    int  s;
+    int  e;
+};
+typedef struct a queue;
+MyType pop(queue* a);
+void push(queue* a,MyType b);
+void initQueue(queue* a);
+int isEmpty(queue* a);
+#endif
